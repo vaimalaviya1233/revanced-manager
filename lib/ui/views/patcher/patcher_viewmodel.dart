3056b11abc55c3edf456dbd43d1cd7bfeb6cbd81
inotia00 <ars258000@gmail.com>
2023-09-05 15:51:05 +0900

    feat: restricted to build only for arm64-v8a architecture

--- lib/ui/views/patcher/patcher_viewmodel.dart
+++ lib/ui/views/patcher/patcher_viewmodel.dart
@@ -122,7 +122,7 @@ class PatcherViewModel extends BaseViewModel {
   Future<void> showArmv7WarningDialog(BuildContext context) async {
     final bool armv7 = await AboutInfo.getInfo().then((info) {
       final List<String> archs = info['supportedArch'];
-      final supportedAbis = ['arm64-v8a', 'x86', 'x86_64'];
+      final supportedAbis = ['arm64-v8a'];
       return !archs.any((arch) => supportedAbis.contains(arch));
     });
     if (context.mounted && armv7) {
